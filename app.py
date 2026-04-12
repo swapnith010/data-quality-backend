67e249bb0eb8e772d268d4af44803d86486225cd
varsha0017 <kondakallavarsha23@ifheindia.org>
2026-04-12 18:10:51 +0530

    fix port issue

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def report():
     return send_file(file, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
